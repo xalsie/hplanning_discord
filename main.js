5cdbaec3cc6bff30fadd8a99fab319495c08998b
Alexis Giard <xalsie.ff@hotmail.fr>
2022-01-12 01:50:51 +0100

    FIX git separed branch

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // ### Structure multi Bots
 // ### LeGrizzli#0341
 
-const bot_1 = require('./home.js');
+// const bot_1 = require('./home.js');
 const bot_2 = require('./redNsfw.js');
-// const bot_2 = require('./comptaHome.js');
+// const bot_3 = require('./comptaHome.js');
+// const bot_4 = require('./web.js');
